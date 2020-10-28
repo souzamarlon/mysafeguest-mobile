018b57b009bbc70afef3c5a88066eac75407c730
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2020-10-28 00:12:17 -0300

    Create the Resident page

--- src/pages/Dashboard/Resident/index.js
+++ src/pages/Dashboard/Resident/index.js
@@ -1,10 +1,61 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 
-// import { Container } from './styles';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Resident = () => {
-  return <View />;
-};
+import { format, parseISO, isBefore } from 'date-fns';
 
-export default Resident;
+import { Container, List, AppointmentInfo, Name } from './styles';
+
+import api from '~/services/api';
+
+export default function Resident({ navigation }) {
+  const [appointments, setAppointments] = useState([]);
+
+  const dispatch = useDispatch();
+  const { id, name } = useSelector((state) => state.user.profile);
+
+  useEffect(() => {
+    async function getAppointments() {
+      try {
+        const response = await api.get(`/appointments/${id}`);
+
+        const dataFormat = response.data.map((item) => ({
+          ...item,
+          start_date: format(parseISO(item.start_date), "MMMM d',' yyyy"),
+          end_date: format(parseISO(item.end_date), "MMMM d',' yyyy"),
+          isActiveDate: isBefore(new Date(), parseISO(item.end_date)),
+        }));
+
+        setAppointments(dataFormat);
+        // setRefreshList(false);
+      } catch (err) {
+        Alert.alert('Unable to get the appointments!');
+      }
+    }
+
+    getAppointments();
+  }, [id]);
+
+  console.tron.log(appointments);
+
+  return (
+    <Container>
+      <List
+        data={appointments}
+        // refreshing={refreshList}
+        // onRefresh={loadPage}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item: data }) => (
+          <AppointmentInfo
+            onPress={() => {
+              navigation.navigate('Appointments', { data });
+            }}
+          >
+            <Name>{data.name}</Name>
+          </AppointmentInfo>
+        )}
+      />
+    </Container>
+  );
+}
--- src/pages/Dashboard/Resident/styles.js
+++ src/pages/Dashboard/Resident/styles.js
@@ -1,3 +1,36 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  margin: 0 auto;
+  /* background: rgba(11, 222, 0, 0.1); */
+  background: rgba(255, 255, 255, 0.4);
+  flex: 1;
+  width: 100%;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingBottom: 20,
+  },
+})``;
+
+export const AppointmentInfo = styled.View`
+  background: rgba(255, 255, 255, 0.6);
+  margin-top: 10px;
+  height: 150px;
+  padding: 2px 20px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+`;
+
+export const Name = styled.Text`
+  color: #333;
+  font-size: 22px;
+  font-weight: bold;
+  margin-left: 5px;
+  text-align: center;
+`;
